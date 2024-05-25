dbfbd9a61a0f60e5afcc770e01aa246c7fc03dd1
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-05-26 02:47:43 +0900

    [feat](SearchTab-GUI): add gui widgets, seperate gui configurations from main

--- SearchTab.py
+++ SearchTab.py
@@ -1,14 +1,59 @@
 from tkinter import *
 from tkinter import messagebox
 
+import GuiConfig
+
 class SearchTab:
     def __init__(self, master):
         self.master = master
         self.frame = Frame(self.master)
         self.frame.pack()
 
-        self.button = Button(self.frame, text="Click Me", command=self.click)
-        self.button.pack()
+        self.searchBar = Frame(self.frame)
+        self.searchBar.place( x=GuiConfig.TABS_PADDINGX, y=GuiConfig.SEARCH_BAR_PADDING_Y,
+            width=GuiConfig.SEARCH_ENTRY_WIDTH + GuiConfig.SEARCH_BUTTON_WIDTH + GuiConfig.WIDGET_INTERVALX,
+            height=max(GuiConfig.SEARCH_ENTRY_HEIGHT, GuiConfig.SEARCH_BUTTON_HEIGHT) + GuiConfig.WIDGET_INTERVALY,
+            anchor=NW
+        )
 
-    def click(self):
-        messagebox.showinfo("Search Tab", "You clicked the button in the Search Tab")
+        self.searchStr = StringVar()
+        self.searchStr.set("검색어를 입력하세요")
+        self.entry = Entry(self.searchBar, textvariable=self.searchStr, font=GuiConfig.cFont)
+        self.entry.place( x=GuiConfig.TABS_PADDINGX, y=0,
+            width=GuiConfig.SEARCH_ENTRY_WIDTH, height=GuiConfig.SEARCH_ENTRY_HEIGHT,
+            anchor=NW
+        )
+
+        self.button = Button(self.searchBar, text="검색", font=GuiConfig.cFont, command=self.search)
+        self.button.place( x=GuiConfig.SEARCH_ENTRY_WIDTH + GuiConfig.WIDGET_INTERVALX,
+            y=0, width=GuiConfig.SEARCH_BUTTON_WIDTH, height=GuiConfig.SEARCH_BUTTON_HEIGHT,
+            anchor=NW
+        )
+
+        self.result = Frame(self.frame, height=GuiConfig.SEARCH_RESULT_HEIGHT)
+        self.result.place( x=GuiConfig.TABS_PADDINGX,
+            y=max(GuiConfig.SEARCH_ENTRY_HEIGHT, GuiConfig.SEARCH_BUTTON_HEIGHT)
+                + GuiConfig.WIDGET_INTERVALY + GuiConfig.SEARCH_BAR_PADDING_Y,
+            width=GuiConfig.SEARCH_RESULT_WIDTH + GuiConfig.VIEW_BUTTON_WIDTH + GuiConfig.WIDGET_INTERVALX,
+            height=GuiConfig.SEARCH_RESULT_HEIGHT,
+            anchor=NW
+        )
+
+        self.resultList = Listbox(self.result, font=GuiConfig.cFont)
+        self.resultList.place( x=0, y=0,
+            width=GuiConfig.SEARCH_RESULT_WIDTH, height=GuiConfig.SEARCH_RESULT_HEIGHT,
+            anchor=NW
+        )
+
+        self.viewButton = Button(self.result, text="View", font=GuiConfig.cFont, command=self.view)
+        self.viewButton.place( x=GuiConfig.SEARCH_RESULT_WIDTH + GuiConfig.WIDGET_INTERVALX,
+            y=GuiConfig.VIEW_BUTTON_PADDINGY,
+            width=GuiConfig.VIEW_BUTTON_WIDTH, height=GuiConfig.VIEW_BUTTON_HEIGHT,
+            anchor=NW                      
+        )
+
+    def search(self):
+        pass
+
+    def view(self):
+        pass
--- main.py
+++ main.py
@@ -7,26 +7,18 @@ from MemoTab import *
 from LogTab import *
 
 from GIFAnimation import *
+import GuiConfig
 
 class MainGUI:
-    GIF_WIDTH = 150
-    GIF_HEIGHT = 100
-    GIF_PADDINGX = 20
-    GIF_PADDINGY = 10
-    TABS_PADDINGX = 20
-    TABS_PADDINGY = 20
-
-    WIDTH = 800
-    HEIGHT = 600
-
     def __init__(self):
         self.master = Tk()
         self.master.title("Main GUI")
-        self.master.geometry(str(MainGUI.WIDTH) + "x" + str(MainGUI.HEIGHT))
+        self.master.geometry(str(GuiConfig.WIDTH) + "x" + str(GuiConfig.HEIGHT))
+        GuiConfig.initFonts()
 
         self.notebook = ttk.Notebook(self.master)
         self.notebook.pack( fill=BOTH, expand=True, side=LEFT,
-            padx=MainGUI.TABS_PADDINGX, pady=MainGUI.TABS_PADDINGY
+            padx=GuiConfig.TABS_PADDINGX, pady=GuiConfig.TABS_PADDINGY
         )
 
         self.searchTab = SearchTab(self.master)
@@ -41,11 +33,11 @@ class MainGUI:
         self.logTab = LogTab(self.master)
         self.notebook.add(self.logTab.frame, text="열람 기록")
 
-        self.gif = GIFAnimation(MainGUI.GIF_WIDTH, MainGUI.GIF_HEIGHT)
+        self.gif = GIFAnimation(GuiConfig.GIF_WIDTH, GuiConfig.GIF_HEIGHT)
         self.gifLabel = Label(self.master, image=self.gif.image())
         self.gifLabel.place(
-            x=MainGUI.WIDTH - MainGUI.GIF_PADDINGX, y=MainGUI.GIF_PADDINGY,
-            anchor=NE, width=MainGUI.GIF_WIDTH, height=MainGUI.GIF_HEIGHT
+            x=GuiConfig.WIDTH - GuiConfig.GIF_PADDINGX, y=GuiConfig.GIF_PADDINGY,
+            anchor=NE, width=GuiConfig.GIF_WIDTH, height=GuiConfig.GIF_HEIGHT
         )
 
         self.master.after(100, self.updateGIF)
