5c21ca13102182346265bcc095e0866113b3468b
orange2458 <asm1133@naver.com>
2024-05-28 01:53:51 +0900

    논문 정보를 가져올 때 필요한 data

---
+++ paper.py
@@ -0,0 +1,7 @@
+import os
+
+paperDataUrl = 'http://apis.data.go.kr/B552540/KCIOpenApi/artiInfo/openApiM310List'
+#testUrl2 = 'http://apis.data.go.kr/B552540/KCIOpenApi/artiInfo/openApiM330List'
+KEY = os.environ['PAPER_API_KEY']
+
+parseCnt = 100
