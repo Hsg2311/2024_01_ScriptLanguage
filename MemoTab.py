5571b0bd725dbbb9fedccfda9b6d5fa07ece4abf
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-05-28 17:08:12 +0900

    [feat](MemoTab-GUI): add Memo class

--- MemoTab.py
+++ MemoTab.py
@@ -1,6 +1,19 @@
 from tkinter import *
 from tkinter import messagebox
 
+class Memo:
+    WIDTH = 24  # number of characters
+    HEIGHT = 8  # number of lines
+
+    def __init__(self, master, text=''):
+        self.master = master
+        self.text = text
+        self.widget = Text(self.master, width = Memo.WIDTH, height = Memo.HEIGHT)
+        self.widget.insert(END, self.text)
+
+    def grid(self, i, j):
+        self.widget.grid(row=i, column=j, padx=10, pady=10)
+
 class MemoTab:
     def __init__(self, master):
         self.master = master
@@ -8,16 +21,13 @@ class MemoTab:
         self.frame.pack()
 
         self.grids = Frame(self.frame)
-        self.grids.place(x=10, y=10, width=680, height=540)
+        self.grids.place(x=10, y=10, width=680, height=580)
 
-        self.memos = []
+        self.memos = [
+            [Memo(self.grids, 'Memo 1'), Memo(self.grids, 'Memo 2')],
+            [Memo(self.grids, 'Memo 3'), Memo(self.grids, 'Memo 4')]
+        ]
 
-        for i in range(5):
-            for j in range(5):
-                tmp = Text(self.grids)
-                tmp.grid(row=i, column=j, padx=10, pady=10)
-                self.memos.append(tmp)
-
-        for i in range(5):
-            self.grids.grid_rowconfigure(i, weight=1)
-            self.grids.grid_columnconfigure(i, weight=1)
+        for i in range(len(self.memos)):
+            for j in range(len(self.memos[i])):
+                self.memos[i][j].grid(i, j)
