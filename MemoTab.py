d310a02c45475a825d194c9111b2394e0c69faa9
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-05-28 16:57:31 +0900

    [feat](MemoTab-GUI): change Entry to Text

--- MemoTab.py
+++ MemoTab.py
@@ -8,14 +8,14 @@ class MemoTab:
         self.frame.pack()
 
         self.grids = Frame(self.frame)
-        self.grids.place(x=10, y=10, width=680, height=580)
+        self.grids.place(x=10, y=10, width=680, height=540)
 
         self.memos = []
 
         for i in range(5):
             for j in range(5):
-                tmp = Entry(self.grids)
-                tmp.grid(row=i, column=j, padx=10, pady=10, sticky="nsew")
+                tmp = Text(self.grids)
+                tmp.grid(row=i, column=j, padx=10, pady=10)
                 self.memos.append(tmp)
 
         for i in range(5):
