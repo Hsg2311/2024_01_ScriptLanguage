2bb3df457e998182fa7c958d3d04c30f593890d1
orange2458 <asm1133@naver.com>
2024-05-29 15:12:39 +0900

    Update xmlParsing.py

--- xmlParsing.py
+++ xmlParsing.py
@@ -1,23 +1,47 @@
-import paper
-import sys
+import papery
 import xml.etree.ElementTree as ET
 import requests
 
-papers = []
-for i in range(1, 21):
-    testParams = {'serviceKey' : paper.KEY, 'recordCnt' : 2, 'pageNo' : i}
-    response = requests.get(paper.paperDataUrl, params=testParams)
-    
-    root = ET.fromstring(response.text)
+class xmlParsing:
+    def __init__(self, key, paperDataURL, parseCnt):
+        self.papers = []
+        self.key = key
+        self.paperDataURL = paperDataURL
+        self.cnt = parseCnt
 
-    for item in root.iter('item'):
-        paper = {
-            'Title' : item.findtext('ARTI_KOR_TITL'),
-            'Abst' : item.findtext('KOR_ABST')
-        }
-        papers.append(paper)
+    def printPapers(self):
+        for paper in self.papers:
+            print('Title :', paper['Title'])
 
-for paper in papers:
-    print('Title :', paper['Title'])
-    print('Abstract :', paper['Abst'])
-    print()
+    def parse(self):
+        for _ in range(1, (self.cnt // 100) + 1):
+            paperDataParams = {'key' : self.key, 'apiCode' : 'articleSearch', 'title' : 1, 'displayCount' : 100}
+            response = requests.get(self.paperDataURL, params=paperDataParams)
+            
+            # f = open('paperData.xml', 'w', encoding='utf-8')
+            # f.write(response.text)
+            # f.close()
+
+            root = ET.fromstring(response.text)
+
+            for item in root.iter('articleInfo'):
+                paper = {
+                    'Title' : self.getTitle(item)
+                }
+                self.papers.append(paper)
+
+    def getTitle(self, item):
+        # 상위 엘리먼트에서부터 찾으면서 내려와야 함.
+        title_group = item.find('title-group')
+        article_title = title_group.find('article-title')
+        print(article_title.text)
+        # title_group = item.findtext('title-group')
+        # if title_group is not None:
+        #     for article_title in item.findall('article-title'):
+        #         if article_title.get('lang') == 'original':
+        #             return article_title.text
+        # return 'fuck'
+
+parser = xmlParsing(papery.KEY, papery.paperDataUrl, 100)
+parser.parse()
+parser.printPapers()
