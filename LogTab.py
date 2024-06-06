4e5adb5ce399f4a47fa174e9f4b06ba6d0e69f98
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-06-06 12:38:48 +0900

    [feat](LogTab-GUI): Config widget positions

--- LogTab.py
+++ LogTab.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 
+import GuiConfig
+
 class LogTab:
     def __init__(self, mainGUI):
         self.mainGUI = mainGUI
@@ -8,12 +10,47 @@ class LogTab:
         
         self.frame = Frame(self.master)
         self.frame.pack()
+        
+        self.dummyFrame = Frame(self.frame)
+        self.dummyFrame.pack(pady=GuiConfig.LOG_PADDINGY // 2)
 
-        self.label = Label(self.frame, text="Log Tab")
-        self.label.pack()
+        self.logFrame = Frame(self.frame)
+        self.logFrame.pack(side=LEFT)
 
-        self.button = Button(self.frame, text="Click Me", command=self.click)
-        self.button.pack()
+        self.searchLogFrame = Frame(self.logFrame)
+        self.searchLogFrame.pack()
+        self.searchLogCanvas = Canvas(self.searchLogFrame, width=GuiConfig.LOG_LOG_WIDTH,
+            height=GuiConfig.LOG_SEARCHLOG_HEIGHT, bg="red"
+        )
+        self.searchLogCanvas.pack()
+
+        self.viewLogFrame = Frame(self.logFrame)
+        self.viewLogFrame.pack(pady=GuiConfig.WIDGET_INTERVALY)
+        self.viewLogCanvas = Canvas(self.viewLogFrame, width=GuiConfig.LOG_LOG_WIDTH,
+            height=GuiConfig.LOG_VIEWLOG_HEIGHT, bg="blue"
+        )
+        self.viewLogCanvas.pack()
+
+        self.viewButtonFrame = Frame(self.frame)
+        self.viewButtonFrame.pack(side=LEFT, padx=GuiConfig.WIDGET_INTERVALX,
+            fill=Y, expand=True
+        )
+
+        self.viewButton = Button(self.viewButtonFrame, text="View",
+            width=GuiConfig.LOG_VIEW_BUTTON_WIDTH, height=5,
+            command=self.click
+        )
+        self.viewButton.pack(pady=GuiConfig.LOG_VIEW_BUTTON_PADDINGY, side=TOP, anchor=N)
+
+
+        self.buildSearchLog()
+        self.buildViewLog()
+
+    def buildSearchLog(self):
+        pass
+
+    def buildViewLog(self):
+        pass
 
     def click(self):
         messagebox.showinfo("Log Tab", "You clicked the button in the Log Tab")
